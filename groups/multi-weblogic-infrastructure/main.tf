10ba0bec2e32160254cfe040f48f5574f6672b76
Jake Metherell <jake@fluffylogic.com>
2025-04-29 08:10:36 +0100

    Reference 1.0.320 of chips-app module to bring in iBoss ingress rules

--- groups/multi-weblogic-infrastructure/main.tf
+++ groups/multi-weblogic-infrastructure/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-app" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=tags/1.0.292"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=tags/1.0.320"
 
   application                        = local.application_name
   application_type                   = var.application_type
