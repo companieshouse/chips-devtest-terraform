ab8e5cc06d718af4b53325a240df8c56eee1e561
makhtar-ch <makhtar@companieshouse.gov.uk>
2025-12-09 18:17:34 +0000

    update module version

--- groups/multi-weblogic-infrastructure/main.tf
+++ groups/multi-weblogic-infrastructure/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-app" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=tags/1.0.359"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=tags/1.0.363"
 
   application                        = local.application_name
   application_type                   = var.application_type
